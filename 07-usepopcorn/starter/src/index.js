e468735b47f6a08cd57c0154e27c51870e94a072
Muadz <muadzyamani@gmail.com>
2023-11-11 13:58:50 +0800

    Include App-v3 as main

--- 07-usepopcorn/starter/src/index.js
+++ 07-usepopcorn/starter/src/index.js
@@ -3,16 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App-v3";
 
-// import StarRating from "./StarRating";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" /> */}
   </React.StrictMode>
 );

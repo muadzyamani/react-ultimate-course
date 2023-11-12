ea8cbbb479fbe6fca2291660dcfb7dd69f546945
Muadz <muadzyamani@gmail.com>
2023-11-12 21:58:54 +0800

    Implement restart quiz system

--- 10-react-quiz/starter/src/components/App.js
+++ 10-react-quiz/starter/src/components/App.js
@@ -49,6 +49,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
     default:
       throw new Error("Unknown action");
   }
@@ -108,6 +110,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>

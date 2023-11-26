76a6bd9c296074dca07d0ebb1271f65035d66386
Muadz <muadzyamani@gmail.com>
2023-11-26 09:35:01 +0800

    Implement deleteCity

--- 11-worldwise/starter/src/contexts/CitiesContext.jsx
+++ 11-worldwise/starter/src/contexts/CitiesContext.jsx
@@ -54,7 +54,23 @@ function CitiesProvider({ children }) {
 
       setCities((cities) => [...cities, data]);
     } catch (error) {
-      alert("There was an error loading data...");
+      alert("There was an error creating city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (error) {
+      alert("There was an error deleting city...");
     } finally {
       setIsLoading(false);
     }
@@ -68,6 +84,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
